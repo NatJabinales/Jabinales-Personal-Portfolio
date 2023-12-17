1a3e009e12a3b60acf27fd39dd80a180a65594ff
NatJabinales <153958442+NatJabinales@users.noreply.github.com>
2023-12-17 22:32:44 +0800

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Nathaniel O. Jabinales</span> a dedicated and passionate BSIT (Bachelor of Science in Information Technology) student driven by a profound interest in the dynamic world of technology. Currently I immersed in the pursuit of knowledge a keen intellect and an unwavering commitment for my academic endeavors. My academic journey is marked by a strong foundation in information technology reflecting my aspiration to contribute meaningfully to the ever-evolving tech landscape. <br><br> Beyond the hard academia I finds joy and fulfillment in the creative world of picture and video editing. My hobby are not only serves as a source of relaxation but also showcasing my artistic flair and attention to detail. I also proficiency in manipulating visual elements extends to my enthusiasm for creating a website design with a keen eye for aesthetics and a natural aptitude for user experience I focus in crafting digital spaces that are both visually appealing and functionally seamless. <br><br> In the fast-paced and ever-evolving field of information technology I keep study hard to stand as a promising individual blending academic prowess with creative acumen to navigates the intricate landscape of IT studies I continues to carve my path to make meaningful contributions to the tech industry with a unique combination of technical expertise and creative ingenuity."
+      ],
+      typeSpeed: -10,
+      contentType: 'html'
+    });
+  }, 100);
